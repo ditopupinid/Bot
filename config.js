0d618a3f4c082fd5f23f7d929fdc0118ae1b5631
onlykape <kpranata141@gmail.com>
2023-10-28 12:33:02 +0700

    bot

---
+++ config.js
@@ -0,0 +1,61 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+
+
+//-----------------------
+// SESSION ID CONECTING 👇🏻
+//-------------------------
+module.exports = {
+SESSION_ID: 'NutmegEmotions', //Put your session id here
+ALIVE: `HELLO I AM ALIVE`
+}
+/*
+
+ Other Configurations are down 👇🏻
+
+
+*/
+//---------------------------------
+// Only use true/false
+//---------------------------------
+global.autoTyping = false //Auto typing...(true for on, false for off)
+global.autoRecord = false // Auto recording...(true for on, false for off)
+global.autoread = false // Auto Message read(true for on, false for off)
+global.alwaysonline = true // Always Online(true for on, false for off)
+global.statusread = true // Auto status read 🗿🖐🏻(true for on, false for off)
+global.private = false // worktype(true for private, false for public)
+global.autoreact = false // Auto react ( verum myra aah 🤣🙌🏻 )
+global.autodlyt = false // Auto YouTube video downloader(true for on, false for off)
+global.largethumb = "true" // for large thumbnail 
+global.version = "1.5.0"
+//---------------------------------
+// Required variables
+//---------------------------------
+global.prefix = "." // use "" for removing prefix
+global.ownername = "0xff_404" // Bot Owner name
+global.botname = "Ciluk Bot" //bot name
+global.thumb = "https://telegra.ph/file/c3668b52e52264996085e.jpg" // bot image overall
+global.ownernumber = ["6289688329976","6282220727243"] //SODO / OWNER NUMBER
+global.caption = "_*ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ciluk-ʙᴏᴛ*_" // Caption 
+global.packname = `0xff_404` //sticker pack
+global.author = "Ciluk Bot by P" //sticker name
+//---------------------------------
+// Owner Mention Audio
+//---------------------------------
+global.ownermention = true // to turn off type false  || turn on type true 👌🏻
+global.mtitle = "Ciluk Bot" // mention tittle
+global.mbody = "0xff_404 is P" // mention discription 
+global.mthumb = 'https://telegra.ph/file/01da195369259e13a8121.jpg' // mention thumbnail 
+global.murl = 'https://www.tiktok.com/@gamehack3rz'
+global.mentionaudio = ["https://i.imgur.com/h0mNy8p.mp4","https://i.imgur.com/zNWjhDn.mp4","https://i.imgur.com/Ya3325t.mp4","https://i.imgur.com/k4FE9mA.mp4","https://i.imgur.com/X5WGE9W.mp4","https://i.imgur.com/veoDgRH.mp4","https://i.imgur.com/CZ3eJgw.mp4","https://i.imgur.com/6QFiAoM.mp4","https://i.imgur.com/lAbt9ew.mp4"] //mention audios
+//---------------------------------
+// All in One Url ( you can add any url you want )
+//---------------------------------
+global.url = 'https://discord.com/invite/P4xccXeXGC' // mention url
+//---------------------------------
+// No need of editing ( Edit aaki oombanda 🗿🖐🏻) kuduthal edit aakiyal kolamm aayum myra 🖐🏻🤣
+//---------------------------------
+global.IMGBB_KEY = ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"]
+global.reactemoji = ['😨','😅','😂','😳','😎', '🥵', '😱', '🐦', '🙄', '🐤','🗿','🐦','🤨','🥴','😐','👆','😔', '👀','👎','🔥','💦','✂️','🍭'] // auto react emoji
+global.sparky = ['☆','✼','　 ҉ ','❏','🍭','△'] // menu design's
