79ae03e7353ce88d7f6b3ba9b879c37d2c467ffa
onlykape <kpranata141@gmail.com>
2023-11-03 20:08:20 +0700

    updaet

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const {
    spawn
 } = require('child_process')
 const path = require('path')
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 function start() {
    let args = [path.join(__dirname, 'alpha.js'), ...process.argv.slice(2)]
